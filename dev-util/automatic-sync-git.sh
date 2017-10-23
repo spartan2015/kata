1c4dc640ee1be319bd95be7cfb6fdc8b0101adcf
Doc MsEco <doc.mseco@gmail.com>
2017-10-23 10:19:10 +0300

    next

--- dev-util/automatic-sync-git.sh
+++ dev-util/automatic-sync-git.sh
@@ -1,2 +1,12 @@
-cd /c/dev/my-git/kata && git add . && git commit -m 'next' && git push origin master
-cd /c/dev/my-git/nodejs && git add . && git commit -m 'next' && git push origin master
+#!/bin/bash
+gitsyncho(){
+	cd $1
+	git config user.name="Doc MsEco"
+	git config user.email=doc.mseco@gmail.com
+	git add .
+	git commit -m 'next' && git push origin master	
+}
+
+gitsyncho /c/dev/my-git/kata
+gitsyncho /c/dev/my-git/nodejs
+gitsyncho /c/dev/my-git/kube

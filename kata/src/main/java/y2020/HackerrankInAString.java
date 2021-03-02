7bcc6a0b1dfc81a28d7ec77bc19100d2dfbae4b4
spartan2015 <irimia.vasile@gmail.com>
2021-03-02 09:37:05 +0200

    next

---
+++ kata/src/main/java/y2020/HackerrankInAString.java
@@ -0,0 +1,28 @@
+package y2020;
+
+public class HackerrankInAString {
+
+    // Complete the hackerrankInString function below.
+    static String hackerrankInString(String s) {
+        char[] hr = "hackerrank".toCharArray();
+        int index = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == hr[index]){
+                index++;
+            }
+            if (index == hr.length){
+                break;
+            }
+        }
+
+        if (index == hr.length){
+            return "YES";
+        }else{
+            return "NO";
+        }
+
+    }
+
+
+}
